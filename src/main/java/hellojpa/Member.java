a317b170839d3e13d3070134508bd4577e6a3233
Yoonho Cha <0119hoya@naver.com>
2025-12-09 15:01:40 +0900

    section 7: 1:1

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -25,6 +25,10 @@ public class Member {
     @JoinColumn(insertable = false, updatable = false)
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
