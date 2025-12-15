1a2dfcfb95e9794beeb7ba91ba295505eeaa5bcd
Yoonho Cha <0119hoya@naver.com>
2025-12-15 14:58:27 +0900

    section 9: 영속성 전이(CASCADE)와 고아 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -25,12 +25,8 @@ public class JpaMain {
             parent.addChild(child2);
 
             em.persist(parent);
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-            findParent.getChildList().remove(0);
+            em.persist(child1);
+            em.persist(child2);
 
             tx.commit();
         } catch (Exception e) {
